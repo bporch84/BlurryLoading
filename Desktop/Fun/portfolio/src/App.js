81ce84f7343407211b6dc09df32416a384cfd238
Barry Porch <bporch@ymail.com>
2021-04-24 00:44:55 -0400

    Finished Nav

---
+++ Desktop/Fun/portfolio/src/App.js
@@ -0,0 +1,54 @@
+import { Route, Switch, NavLink } from "react-router-dom";
+
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Contacts from "./components/Contacts";
+import Projects from "./components/Projects";
+
+function App() {
+  return (
+    <div className="app-container">
+      <nav>
+        <h1>Porch's Portfolio</h1>
+        <div className="nav-links">
+          <NavLink
+            exact to="/about"
+            activeClassName="active"
+            className="nav-link"
+          >
+            About
+          </NavLink>
+          <NavLink
+            exact to="/projects"
+            activeClassName="active"
+            className="nav-link"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            exact to="/blog"
+            activeClassName="active"
+            className="nav-link"
+          >
+            Blog
+          </NavLink>
+          <NavLink
+            exact to="/contacts"
+            activeClassName="active"
+            className="nav-link"
+          >
+            Contacts
+          </NavLink>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/contacts" component={Contacts} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
