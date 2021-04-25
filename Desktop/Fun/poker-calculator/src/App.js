19169f4af94311dc2be25f21f6dbd4b38249c246
Barry Porch <bporch@ymail.com>
2021-04-25 13:32:39 -0400

    WIP

--- Desktop/Fun/poker-calculator/src/App.js
+++ Desktop/Fun/poker-calculator/src/App.js
@@ -8,38 +8,39 @@ function App() {
   return (
     <div className="app-container">
       <h1>EV Poker Calculator</h1>
+      <div>
+        <p>To use this EV poker calculator, all you need to do is fill out the three fields:</p>
+        <ul>
+          <li>Win Percentage: The percentage of time you will win the hand.</li>
+          <li>Money Won: The amount you will win if you win the hand.</li>
+          <li>Money Lost: The amount you will lose if you lose the hand.</li>
+        </ul>
+        <p>The formula to determine EV is the percentage of times you are going to win the hand multiplied by the amount of money you will win minus the percentage of times you are going to lose the hand multiplied by the amount of money you will lose.</p>        
+      </div>
       <form onSubmit={handleSubmit}>
-        <div className="label-input">
-          <div className="win-percent">
-            <label>Win Percentage: </label>
-            <input
-              type="text"
-              name="winPercent"
-              value={values.winPercent}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="money-won">
-            <label>Money Won: </label>
-            <input
-              type="text"
-              name="moneyWon"
-              value={values.moneyWon}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="money-lost">
-            <label>Money Lost: </label>
-            <input
-              type="text"
-              name="moneyLost"
-              value={values.moneyLost}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
+        <input
+          type="text"
+          name="winPercent"
+          placeholder="Win Percentage (e.g. 50)"
+          value={values.winPercent}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="moneyWon"
+          placeholder="Money Won (e.g. 160.50)"
+          value={values.moneyWon}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="moneyLost"
+          placeholder="Money Lost (e.g. 65.75)"
+          value={values.moneyLost}
+          onChange={handleChange}
+        />
         <button>Submit</button>
-        <h2>EV: {values.ev}</h2>
+        <h2>EV: <span className={`${Math.sign(values.ev) === +1 || Math.sign(values.ev) === +0 ? "positive-ev" : "negative-ev"}`}>{values.ev}</span></h2>
       </form>
     </div>
   );
